e0bb5098c0ef85c826c1d5ccabdbad4b04b6a11b
marcdev30 <programarc30@gmail.com>
2023-01-25 20:49:20 +0100

    Add mp4 extension

--- modules/validators.js
+++ modules/validators.js
@@ -1,22 +1,30 @@
 const path = require("path");
 
-exports.isValid = (file) => {
-    return (validateSize(file) && validateType(file));
-}
+exports.isValid = file => {
+	return validateSize(file) && validateType(file);
+};
 
 function validateSize(file) {
-    if(file.size <= 5000000) {
-        return true;
-    } else {
-        return false;
-    }
+	if (file.size <= 5000000) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function validateType(file) {
-    const extensions = ['.png', '.jpg', '.jpeg', '.svg', '.gif', '.raw']; 
-    if(extensions.includes(path.extname(file.name))) {
-        return true;
-    } else {
-        return false;
-    }
+	const extensions = [
+		".png",
+		".jpg",
+		".jpeg",
+		".svg",
+		".gif",
+		".raw",
+		".mp4",
+	];
+	if (extensions.includes(path.extname(file.name))) {
+		return true;
+	} else {
+		return false;
+	}
 }
