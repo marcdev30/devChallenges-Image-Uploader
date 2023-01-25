fc0ea53b5e8a5f9f51ebe367d2db35513b166d71
marcdev30 <programarc30@gmail.com>
2023-01-25 21:23:06 +0100

    Solve find name

--- modules/fileName.js
+++ modules/fileName.js
@@ -2,40 +2,58 @@ const fs = require("fs");
 const path = require("path");
 
 exports.generateName = () => {
-    var randomI1 = Math.floor(Math.random() * (100000));
-    const toAdd1 = (5 - randomI1.toString().length) * '0';
-    randomI1 = (randomI1.toString().length === 5) ? randomI1 : toAdd1 + randomI1.toString();
+	var randomI1 = Math.floor(Math.random() * 100000);
+	const toAdd1 = (5 - randomI1.toString().length) * "0";
+	randomI1 =
+		randomI1.toString().length === 5
+			? randomI1
+			: toAdd1 + randomI1.toString();
 
-    var randomI2 = Math.floor(Math.random() * (100000));
-    const toAdd2 = (5 - randomI2.toString().length) * '0';
-    randomI2 = (randomI2.toString().length === 5) ? randomI2 : toAdd2 + randomI2.toString();
+	var randomI2 = Math.floor(Math.random() * 100000);
+	const toAdd2 = (5 - randomI2.toString().length) * "0";
+	randomI2 =
+		randomI2.toString().length === 5
+			? randomI2
+			: toAdd2 + randomI2.toString();
 
-    const fileName = "photo-" + randomI1 + "-" + randomI2;
+	const fileName = "photo-" + randomI1 + "-" + randomI2;
 
-    if(exports.findFile(fileName) === null) {
-        return fileName;
-    } else {
-        const newName = exports.generateName();
-        return newName;
-    }
-}
+	if (exports.findFile(fileName) === null) {
+		return fileName;
+	} else {
+		const newName = exports.generateName();
+		return newName;
+	}
+};
 
-exports.findFile = (imageName) => {
+exports.findFile = imageName => {
+	let extFound = "";
+	const extensions = [
+		".png",
+		".jpg",
+		".jpeg",
+		".svg",
+		".gif",
+		".raw",
+		".mp4",
+		".mpeg",
+	];
+	const samplePath = path.join(
+		path.normalize(__dirname + "/.."),
+		"public",
+		"files",
+		imageName
+	);
 
-    let extFound = '';
-    const extensions = ['.png', '.jpg', '.jpeg', '.svg', '.gif', '.raw'];
-    const samplePath = path.join(path.normalize(__dirname + '/..'), "public", "files", imageName);
+	extensions.forEach(extension => {
+		if (fs.existsSync(samplePath + extension)) {
+			extFound = extension;
+		}
+	});
 
-    extensions.forEach(extension => {
-        if (fs.existsSync(samplePath + extension)) {
-            extFound = extension;
-        }
-    });
-
-    if(extFound != '') {
-        return samplePath + extFound;
-    } else {
-        return null;
-    }
-
-}
+	if (extFound != "") {
+		return samplePath + extFound;
+	} else {
+		return null;
+	}
+};
